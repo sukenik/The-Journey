df0b911dc7ff6acab4475d2ea984474c2d01cab9
Daniel Sukenik <sukenik9@gmail.com>
2022-02-03 14:23:28 +0200

    Created Assets folder
    Moved icons to Assets therefore changed icon paths

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const itemOneTitle = document.querySelector('#item-1-title');
 const itemOneIcon = document.querySelector('#item-1-icon');
 const itemsList = document.querySelectorAll('.items');
 
-fetch('./hafifa.json')
+fetch('data.json')
     .then(function(response) {
         return response.json();
     })
@@ -19,25 +19,25 @@ function matchIconToType(JSONdataList) {
         let img = document.createElement("img");
         switch(iconType) {
             case "unicorn":
-                img.src = "unicorn-g8421e3a51_640.png";
+                img.src = "Assets/unicorn-g8421e3a51_640.png";
                 break;
             case "dove":
-                img.src = "dove-g1c8d3200e_640.png";
+                img.src = "Assets/dove-g1c8d3200e_640.png";
                 break;
             case "alligator":
-                img.src = "alligator-gbcc9b6ad5_640.png";
+                img.src = "Assets/alligator-gbcc9b6ad5_640.png";
                 break;
             case "postgres":
-                img.src = "baby-elephant-g721c6ce8d_640.png";
+                img.src = "Assets/baby-elephant-g721c6ce8d_640.png";
                 break;
             case "python":
-                img.src = "snake-gb30a86d56_640.png";
+                img.src = "Assets/snake-gb30a86d56_640.png";
                 break;
             case "mysql":
-                img.src = "dolphin-gc797926dc_640.png";
+                img.src = "Assets/dolphin-gc797926dc_640.png";
                 break;
             case "beaver":
-                img.src = "beaver-g9672e2117_640.png";
+                img.src = "Assets/beaver-g9672e2117_640.png";
             default:
                 break;
         }
